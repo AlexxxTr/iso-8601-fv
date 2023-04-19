2b7a55e9dbb677917fc106324915578bad6a2622
Alexander Truyaert <alexander.truyaert@outlook.com>
2023-04-19 20:49:49 +0200

    Added parsing for Calendar Dates

--- formatting/index.ts
+++ formatting/index.ts
@@ -1,1 +1,19 @@
-import { } from '../types';
+import { DateParamter, datesNotation } from '../types';
+
+export function parseDate(date: DateParamter, form: datesNotation): string {
+  if (typeof date === 'number') date = new Date(date);
+  const parts = Intl.DateTimeFormat(undefined, {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  })
+    .formatToParts(date)
+    .filter((part) => part.type !== 'literal');
+
+  return form
+    .replace('YYYY', parts.at(2)?.value ?? date.getFullYear().toString())
+    .replace('MM', parts.at(1)?.value ?? date.getMonth().toString())
+    .replace('DD', parts.at(0)?.value ?? date.getDay().toString());
+}
+
+console.log(parseDate(new Date(), 'YYYYMMDD'));
--- types.ts
+++ types.ts
@@ -1,21 +1,23 @@
 // Const arrays (helpers for types);
-const yearNotation = 'YYYY';
-const calendarDatesNotation = ['YYYY-MM-DD', 'YYYY-MM', 'YYYYMMDD'] as const;
-const weekDatesNotation = [
+export const yearNotation = 'YYYY';
+export const calendarDatesNotation = ['YYYY-MM-DD', 'YYYY-MM', 'YYYYMMDD', 'YYYYMM'] as const;
+export const weekDatesNotation = [
   'YYYY-Www',
   'YYYYWww',
   'YYYY-Www-D',
   'YYYYWwwD',
 ] as const;
-const ordinalDatesNotation = ['YYYY-DDD', 'YYYYDDD'] as const;
+export const ordinalDatesNotation = ['YYYY-DDD', 'YYYYDDD'] as const;
 
-const extendedFullTimeNotation = 'T[hh]:[mm]:[ss]';
-const extendedMediumTimeNotation = 'T[hh]:[mm]';
-const basicFullTimeNotation = 'T[hh][mm][ss]';
-const basicMediumTimeNotation = 'T[hh][mm]';
-const shortTimeNotation = 'T[hh]';
+export const extendedFullTimeNotation = 'T[hh]:[mm]:[ss]';
+export const extendedMediumTimeNotation = 'T[hh]:[mm]';
+export const basicFullTimeNotation = 'T[hh][mm][ss]';
+export const basicMediumTimeNotation = 'T[hh][mm]';
+export const shortTimeNotation = 'T[hh]';
 
 // Types
+export type DateParamter = number | Date;
+
 // Dates
 export type yearNotation = typeof yearNotation;
 export type calendarDatesNotation = (typeof calendarDatesNotation)[number];
